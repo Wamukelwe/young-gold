ea9ccba26bdbf51e88a3cc16d3ae9f3470b5caa8
Wamukelwe <95614686+Wamukelwe@users.noreply.github.com>
2025-04-30 12:10:20 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import styles from './page.module.css'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/context/ThemeContext'
 
-const Layout = ({children}) => {
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Young Gold Polisher',
+  description: 'This is the description',
+}
+
+export default function RootLayout({ children }) {
   return (
-    <div className={styles.layoutContainer}>
-      <h1 className={styles.mainTitle}>Our Works</h1>
-      {children}
-    </div>
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
 
-export default Layout
-}
